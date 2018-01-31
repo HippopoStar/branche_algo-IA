1035339fa3c1f6fc5d9d820b98ca9789b1e87655
Leo CABANES <lcabanes@student.42.fr>
2018-01-31 19:35:09 +0100

    Commencement de "ft_printf"

--- ft_printf_folder/ft_printf.c
+++ ft_printf_folder/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 17:58:40 by lcabanes          #+#    #+#             */
-/*   Updated: 2018/01/31 18:55:59 by lcabanes         ###   ########.fr       */
+/*   Updated: 2018/01/31 19:30:36 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,30 @@
 int		ft_printf(const char *format, ...)
 {
 	va_list	ap;
+	size_t	retour;
+	char	c;
 	size_t	i;
 
 	va_start(ap, format);
 	i = 0;
-	while (*(format + i) != '\0')
+	while ((c = *(format + i)) != '\0')
 	{
-		if (*(format + i) == '{')
+		if (c == '{' && (retour = colors((char *)(format + i))))
+			i = i + retour;
+		else if (c == '%' && (retour = flags((char *)(format + i), ap)))
+			i = i + retour;
+		if ((c == '{' || c == '%') && retour == 0)
 		{
-			i = i + colors((char *)(format + i));
+			write(1, (format + i), 1);
+			i++;
 		}
-		if (*(format + i) == '%')
+		retour = 0;
+		while ((c = *(format + i + retour)) != '\0' && c != '{' && c != '%')
 		{
-			i = i + flags((char *)(format + i), ap);
+			retour++;
 		}
-		write(1, (format + i), 1);
-		i++;
+		write(1, (format + i), retour);
+		i = i + retour;
 	}
 	va_end(ap);
 	return (0);
