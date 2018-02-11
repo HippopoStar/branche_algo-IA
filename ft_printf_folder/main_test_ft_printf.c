569d55396d9d397d7254189fa324af8c900c99e7
Leo CABANES <lcabanes@student.42.fr>
2018-02-11 15:55:49 +0100

    Commencement de "ft_printf"

--- ft_printf_folder/main_test_ft_printf.c
+++ ft_printf_folder/main_test_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/31 17:52:23 by lcabanes          #+#    #+#             */
-/*   Updated: 2018/02/09 09:23:48 by lcabanes         ###   ########.fr       */
+/*   Updated: 2018/02/11 15:48:59 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,35 @@
 
 int		main(int argc, char **argv)
 {
-//	t_pf	TEST_PF;
-	char	*a;
-	int		b;
-	char	c;
+	char			*a;
+	long long int	b;
+	char			c;
 
 	a = "42Born2Code";
-	b = -2147483648;
+	b = LLONG_MIN;
 	c = 'y';
-	if (argc != 2)
+	if (argc < 2)
 	{
 		ft_putstr("Passez en argument au programme la phrase a ecrire");
+		return (0);
 	}
-	else
+	if (argc > 2)
 	{
-		ft_printf("Patate\n");
-		ft_printf(*(argv + 1), a, b, c);
-		ft_putchar('\n');
-		printf(*(argv + 1), a, b, c);
-		ft_putchar('\n');
-		printf("% 20.15ld", -2147483648);
-		ft_putchar('\n');
+		a = *(argv + 2);
+		if (argc > 3)
+		{
+			b = ft_atoi(*(argv + 3));
+			if (argc > 4)
+			{
+				c = *(*(argv + 4) + 0);
+			}
+		}
 	}
+	ft_printf(*(argv + 1), a, b, c);
+	ft_putchar('\n');
+	printf(*(argv + 1), a, b, c);
+	printf("\n");
+	printf("LLONG_MIN vaut : %lld", LLONG_MIN);
+	printf("\n");
 	return (0);
 }
