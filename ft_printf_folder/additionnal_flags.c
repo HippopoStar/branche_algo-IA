2aa79f66efe56f34a6410f41da8630b5b352d60f
Leo CABANES <lcabanes@student.42.fr>
2018-02-13 07:39:08 +0100

    Commencement de "ft_printf"

--- ft_printf_folder/additionnal_flags.c
+++ ft_printf_folder/additionnal_flags.c
@@ -6,7 +6,7 @@
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 06:03:16 by lcabanes          #+#    #+#             */
-/*   Updated: 2018/02/13 07:32:20 by lcabanes         ###   ########.fr       */
+/*   Updated: 2018/02/13 07:38:49 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,16 @@ ssize_t	detect_mnoz(char *str)
 
 void	additionnal_flags(char *str, size_t length, t_pf *mai)
 {
-	size_t	c_v;
+	char	c_v;
 	size_t	retour;
 
 	(void)mai;
 	(void)length;
 
-	c_v = go_to_conv_flags(str);
+	c_v = *(str + go_to_conv_flags(str));
 	if (find_flag('#', str) > 0)
 	{
-		p_sharp_mark(*(str + c_v), mai);
+		p_sharp_mark(c_v, mai);
 	}
 	if (find_flag('-', str) > 0)
 	{
@@ -40,7 +40,11 @@ void	additionnal_flags(char *str, size_t length, t_pf *mai)
 	{
 
 	}
-	if ((retour = find_flag(' ', str)) > 0)
+	if ((retour = find_flag('+', str)) > 0)
+	{
+
+	}
+	else if ((retour = find_flag(' ', str)) > 0)
 	{
 
 	}
