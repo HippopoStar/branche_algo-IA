29a728d1c3c309521df12711ed3f0549650e07db
Hephaistos <leocabanes@wanadoo.fr>
2018-02-15 06:50:42 +0100

    Commencement de "ft_printf"

--- ft_printf_folder/additionnal_flags.c
+++ ft_printf_folder/additionnal_flags.c
@@ -104,7 +104,7 @@ size_t	find_flag(char c, char *str)
 	flags[5] = '\0';
 	i = 0;
 	while (occurs(*(str + i), flags) || occurs(*(str + i), "123456789")
-			|| (*(str + i) == '0' && c != '0'))
+			|| (*(str + i) == '0' && !(occurs(*(str + i - 1), "0123456789")) && c != '0'))
 	{
 		i++;
 	}
