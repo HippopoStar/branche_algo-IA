3f0d5cd51d499c90d6d7212afd40f66d20229a1e
Leo CABANES <lcabanes@student.42.fr>
2019-01-25 17:09:27 +0100

    Renouveau de "projet_push_swap" : c'est lent mais au moins ca trie

--- projet_push_swap/srcs_push_swap/ps_sort.c
+++ projet_push_swap/srcs_push_swap/ps_sort.c
@@ -6,7 +6,7 @@
 /*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/19 02:14:32 by lcabanes          #+#    #+#             */
-/*   Updated: 2019/01/23 13:23:01 by lcabanes         ###   ########.fr       */
+/*   Updated: 2019/01/25 17:08:15 by lcabanes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	ps_aux_distribute(t_ps *data, size_t start, size_t end, char c)
 //	ft_putstr("ps_aux_distribute\n");		//TODO
 	tmp = ((c == 'a') ? *(data->stacks + 0) : *(data->stacks + 1));
 	pivot = *(data->sorted + ((start + end + 1) / 2));
+/*
 	ft_putstr("stack: ");					//TODO
 	ft_putchar((c == 'a') ? 'A' : 'B');		//TODO
 	ft_putchar('\n');						//TODO
@@ -32,7 +33,8 @@ void	ps_aux_distribute(t_ps *data, size_t start, size_t end, char c)
 	ft_putstr("pivot: ");					//TODO
 	ft_putnbr(pivot);						//TODO
 	ft_putchar('\n');						//TODO
-	while (tmp != NULL && tmp->next != NULL && !((c == 'a') ? ps_is_a_sorted(data) : ps_is_b_sorted(data)) && ps_gotta_push(tmp, pivot, c))
+*/
+	while (tmp != NULL && tmp->next != NULL && !((c == 'a') ? ps_is_a_sorted(data) : ps_is_b_sorted(data)) && (ps_gotta_push(tmp, pivot, c) || tmp->next->next == NULL))
 	{
 		if (tmp->nb < pivot)
 		{
@@ -64,6 +66,12 @@ void	ps_aux_sort(t_ps *data, size_t start, size_t end, size_t *occ)
 			ps_print_and_apply(data, "pa");
 		}
 	}
+	else if ((end - start) > 2)
+	{
+		ps_distribute(data, start, end, (((*occ) % 2) == 0 ? 'a' : 'b'));
+		ps_aux_sort(data, start, (start + (3 * end) - 1) / 4, occ);
+		ps_aux_sort(data, ((3 * start) + end + 1) / 4, end, occ);
+	}
 	else if ((end - start) > 0)
 	{
 		ps_distribute(data, start, end, (((*occ) % 2) == 0 ? 'a' : 'b'));
@@ -77,8 +85,8 @@ void	ps_sort(t_ps *data)
 	size_t	occ;
 
 //	ft_putstr("ps_sort\n");					//TODO
-	ps_display_stacks(data->stacks);		//TODO
-	occ = 1;
+//	ps_display_stacks(data->stacks);		//TODO
+	occ = 0;
 	ps_aux_sort(data, 0, data->length - 1, &occ);
-	ps_display_stacks(data->stacks);		//TODO
+//	ps_display_stacks(data->stacks);		//TODO
 }
