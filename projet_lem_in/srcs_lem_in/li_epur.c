48f69ca05414aa5d1778ee5e931270f5ab67cba0
Hephaistos <leocabanes@wanadoo.fr>
2019-06-22 17:29:10 +0200

    Ajout ebauche de "li_epur.c"

---
+++ projet_lem_in/srcs_lem_in/li_epur.c
@@ -0,0 +1,44 @@
+#include "lem_in.h"
+
+/*
+**      F - G
+**     /    |\       .V
+**    E     H I
+**    |      \|
+**    D       J        Q
+**     \     /        /
+**      C - K    O - P
+**     /        /
+** A - B - M - N - R - Z
+**        / \
+**       S - T
+**            \      W - X
+**             U      \ /
+**                     Y
+*/
+/*
+** Etape 1:
+** - eliminer les salles isolees [V]
+** Etape 2:
+** - eliminer recursivement les salles n'ayant qu'une liaison [Q-P-O | U]
+**    (si le nombre de liaisons de la salle atteinte '- 1' vaut :
+**    '0' : l'eliminer
+**    '1' : l'eliminer et poursuivre
+**    '+' : s'arreter
+** Etape 3:
+** - eliminer les boucles
+**     Pour chaque salle :
+**     - Tant qu'il existe un couple de laisons
+**       | OU | Si la salle possede uniquement 2 liaisons
+**       ... vers des salles voisines comportant
+**           chacune 1 seule autre liaison [G | S | W]
+**         - Tant que les salles atteintes par chacun des 2 itineraires
+**           ne sont pas confondues (condition necessaire, voir [W-X-Y]),
+**           et qu'elles ne possedent qu'une seule autre liaison
+**             - Suivre ces liaisons [G->H->J ; G->I->J | S->M ; S->T->M | W[...]]
+**         - Si elles rejoignent toutes 2 une meme salle
+**           ou qu'elle se sont toutes les 2 rejointes
+**             - Eliminer le plus long trajet vers cette meme salle [I | T | W[...]]
+**
+** Repeter les etapes 2 et 3 autant de fois que necessaire
+*/
