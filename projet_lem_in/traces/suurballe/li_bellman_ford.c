b8d73b19ff5519765e31731ce53084c643854b0c
Hephaistos <leocabanes@wanadoo.fr>
2019-07-01 01:21:14 +0200

    Ajout du dossier "traces/suurballe/"

---
+++ projet_lem_in/traces/suurballe/li_bellman_ford.c
@@ -0,0 +1,232 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   li_bellman_ford.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/17 17:33:06 by lcabanes          #+#    #+#             */
+/*   Updated: 2019/06/30 18:07:38 by lcabanes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+/*
+** Si on a atteint la salle suivante
+** (en lui ayant decouvert un nouveau poids minimum)
+** en passant par une liaison de poids positif,
+** on place la valeur de sa variable 'except' sur '0',
+** en revanche si on l'a atteinte en passant par une liaison de poids negatif,
+** on place la valeur de sa variable 'except' sur '1'
+**
+** (Notez qu'on ne peut atteindre de salle en passant par une liaison
+** de poids nul)
+**
+** Une salle ne peut posseder plus de 1 liaison de poids negatif, car :
+** - Dans une salle possedant une liaison de poids negatif :
+**     - emprunter une liaison de poids negatif dans un nouvel itineraire
+**       la detruit
+**     - emprunter une liaison de poids positif alors qu'on possede par ailleurs
+**       une liaison de poids negatif, detruit cette liaison
+**       (la liaison de poids negatif resultante sera dans la salle atteinte)
+** - Dans une salle ne possedant pas de liaison de poids negatif :
+**     - atteindre une salle possedant une liaison de poids negatif
+**       pre-existante force cette derniere a l'emprunter, la detruisant ainsi
+**
+** Tel quel, Bellman-Ford ne preserve pas de l'apparition de cycles de poids
+** strictement negatif,
+** et ajouter une condition '&& !((*(data->map + target))->ancestor == i)'
+** fait perdre l'assurance de decouvrir l'itineraire le plus court
+**
+** La condition '&& !(i == target)' on a deja l'assurance que ce n'est pas le
+** cas grace a 'li_match_pipe'
+**
+** La condition '!(*((*(data->map + i))->pipes + target) == 0)', quant a elle,
+** est necessaire, car elle permet de s'assurer que la liaison n'a pas ete
+** fermee a la suite de la decouverte anterieure d'un itineraire
+**
+** Remarque :
+** Etant donne le fait que Bhandari s'assure d'avoir trouver la combinaison de
+** chemin la plus courte a chaque etape, le poids de la salle de depart ne peut
+** descendre en dessous de '0' apres avoir ete atteint par une suite de liaison
+** de poids strictement negatifs, car pour arriver au depart de cette suite de
+** liaisons, il aura fallut emprunter un itineraire de poids superieur a la
+** valeur absolue de cet suite liaison
+**
+** (8 lignes)
+**				if (!(ft_strcmp((*(data->map + i))->name, "Bzd2")
+					  || ft_strcmp((*(data->map + target))->name, "O_z2")))
+**				{
+**					ft_putstr("\033[33maux_li_bellman_ford\033[00m\n");
+**					li_display_room_info(data, (*(data->map + i))->ancestor);
+**					li_display_room_info(data, i);
+**					li_display_room_info(data, target);
+**				}
+*/
+
+/*
+** void	li_suurballe(t_data *data, size_t i, size_t target);
+*/
+
+void	li_suurballe(t_data *data, size_t i, size_t target)
+{
+	int		tmp_wit;
+	size_t	tmp_wei;
+	size_t	forced;
+	size_t	j;
+
+	j = 0;
+	while (j < (*(data->map + target))->nb_of_bonds)
+	{
+		forced = *((*(data->map + target))->bond_sum + j);
+		if (*((*(data->map + target))->pipes + forced) == (signed char)(-1))
+		{
+			tmp_wit = data->wit;
+			data->wit = 0;
+			tmp_wei = (*(data->map + target))->weight;
+			(*(data->map + target))->weight = (*(data->map + i))->weight - 1;
+			li_ping_neighbour(data, target, forced);
+			(*(data->map + target))->weight = tmp_wei;
+			if (data->wit == 1)
+			{
+				(*(data->map + target))->ancestor = i; //Hmmm...
+			}
+			data->wit = (tmp_wit || data->wit) ? 1 : 0;
+		}
+		j++;
+	}
+}
+
+/*
+** L'appel de 'li_ping_neighbour' dans 'li_tunnel_back'
+** ne va pas changer la valeur de 'target_2->ancestor'
+** car a present 'li_ping_neighbour' n'impact que les
+** salles atteintes grace a une liaison de poids '1'
+**
+** Nul besoin de stocker le poids initial de 'target'
+**
+** Cependant, il est important que '(*(data->map + 0))->ancestor == 0'
+**
+** 'i'->'target' : wei++;
+** 'target'->'ancestor' : wei--;
+**	wei = (*(data->map + i))->weight;
+**
+** La fonction 'li_tunnel_back' necessite de ne pas remettre les
+** variables '(t_room *)->ancestor' a 'data->size' pour les
+** salles dont la variable 'allowed' est a '0',
+** dans le corps de la fonction 'li_initialise_weights'
+** De meme pour les variables '(t_room *)->weights' !
+**
+** Par ailleurs, pour le bon fonctionnement de 'li_tunnel_back',
+** il ne faut pas changer les variables '(t_room *)->ancestor' des salles
+** intervenants dans un chemin prealable, et par consequent ne pas appeler
+** 'li_ping_neighbour' dans le cas ou on atteint une salle dont la
+** variable '(t_room *)->allowed' est a '0'
+*/
+
+/*
+** void	li_tunnel_back(t_data *data, size_t i, size_t target);
+*/
+
+/*
+** Il n'est pas cense etre possible de passer le poids de la salle de depart
+** en negatif, car cela impliquerait qu'on ait fait plus de trajet en remontant
+** un ancien chemin, que de trajet pour rejoindre la salle de depart de ce
+** roll-back, or cela impliquerait que s'ait ete par ce 2nd trajet en question
+** qu'il eut fallu passer lors de la decouvert du 1er afin d'obtenir
+** l'itineraire le plus court
+*/
+
+void	li_ping_neighbour(t_data *data, size_t i, size_t target)
+{
+	int		wei;
+
+	if (!(*((*(data->map + i))->pipes + target) == 0)
+			&& ((wei = (*(data->map + i))->weight
+					+ (int)(*((*(data->map + i))->pipes + target)))
+				< (*(data->map + target))->weight))
+	{
+		if ((*(data->map + target))->allowed == 0 && (*(data->map + i))->allowed == 1)
+		{
+			li_suurballe(data, i, target);
+		}
+//		if ((*(data->map + i))->allowed == 0 || (*(data->map + target))->allowed == 1)
+		else
+		{
+			(*(data->map + target))->ancestor = i;
+			(*(data->map + target))->weight = wei;
+			data->wit = 1;
+		}
+	}
+}
+
+/*
+** Au vu de la condition d'appel de 'li_ping_neighbour',
+** il est important que la variable
+** '(*(data->map + data->size - 1))->allowed' ne soit pas
+** mise a '0' dans 'li_reverse_path'
+*/
+
+void	aux_li_bellman_ford(t_data *data, size_t i)
+{
+	size_t	j;
+	size_t	target;
+
+	j = 0;
+	while (j < (*(data->map + i))->nb_of_bonds)
+	{
+		target = *((*(data->map + i))->bond_sum + j);
+		if (!(ft_strcmp((*(data->map + i))->name, "Bzd2") || ft_strcmp((*(data->map + target))->name, "O_z2")))
+		{
+			ft_putstr("\033[33maux_li_bellman_ford\033[00m\n");
+			li_display_room_info(data, (*(data->map + i))->ancestor);
+			li_display_room_info(data, i);
+			li_display_room_info(data, target);
+		}
+		li_ping_neighbour(data, i, target);
+		j++;
+	}
+}
+
+/*
+** On ne peut avoir 'data->size == 0'
+** (car alors 'li_parse_rooms' renverrait une erreur)
+**
+** Si le poids d'une salle vaut 'data->size',
+** c'est qu'elle n'a pas encore ete atteinte depuis l'appel
+** de 'li_initialise_weights'
+**
+** Avec l'arrivee de 'li_suurballe', la condition
+** 'if (!((*(data->map + i))->weight == data->size))'
+** est a mediter
+**
+** L'appel initial 'aux_li_bellman_ford(data, 0)'
+** est place avant la boucle car il est pertinent de
+** le lancer une seule fois, etant donne que le poids de la
+** salle de depart ne va pas etre amene a changer,
+** et il permet d'initialiser la variable 'data->wit' a '1'
+*/
+
+void	li_bellman_ford(t_data *data)
+{
+	size_t	iteration;
+	size_t	i;
+
+	aux_li_bellman_ford(data, 0);
+	iteration = 0;
+	while (iteration < data->size && data->wit == 1)
+	{
+		data->wit = 0;
+		i = 1;
+		while (i < data->size - 1)
+		{
+			if (!((*(data->map + i))->weight == (int)data->size))
+			{
+				aux_li_bellman_ford(data, i);
+			}
+			i++;
+		}
+		iteration++;
+	}
+}
---
+++ projet_lem_in/traces/suurballe/main_lem_in.c
@@ -0,0 +1,277 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_lem_in.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/22 10:02:14 by lcabanes          #+#    #+#             */
+/*   Updated: 2019/06/30 19:12:40 by lcabanes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+
+/*
+** Schema de fonctionnement du programme :
+** | li_options
+**     | ft_options
+**         | aux_ft_options
+**             | ft_strchr (L)
+**     | li_get_output (R)
+**         | ft_strlen (L)
+**         | li_print_output
+** | li_help (O)
+**     | ft_putstr (L)
+**     | li_usage
+**         |ft_putstr (L)
+** | li_usage
+**     | ft_putstr (L)
+** | li_parse_input
+**     | li_parse_ants
+**         | li_get_next_non_comment_line (*)
+**         | li_match_ants
+**             | ft_is_int (L)
+**         | free (S)
+**     | li_parse_rooms
+**         | li_allocate_room (*)
+**         | li_get_next_non_comment_line (*)
+**         | li_match_room
+**             | li_deal_sharp_mark
+**                 | ft_strcmp (L)
+**                 | free (S)
+**             | aux_li_match_room
+**                 | ft_is_int (L)
+**             | li_is_name_available
+**                 | ft_strcmp (L)
+**             | free (S)
+**     | li_allocate_map (*)
+**         | aux_li_allocate_map
+**     | free (S)
+**     | li_parse_pipes
+**         | li_get_next_non_comment_line (*)
+**         | li_match_pipe
+**             | ft_strcmp (L)
+**             | li_actualise_map
+**         | free (S)
+**     | li_shorten_map_pipes (*)
+**         | li_fill_bond_summary
+** | li_print_output
+** | lem_in
+**     | li_bhandari
+**         | li_bhandari_max_iteration
+**         | li_allocate_paths (*)
+**         | li_allocate_routes (*)
+**             | aux_li_allocate_routes
+**         | li_order_rooms
+**         | li_initialise_weights
+**         | li_bellman_ford
+**             | aux_li_bellman_ford
+**                 | li_ping_neighboor
+**         | li_reverse_path
+**         | li_build_route
+**             | li_copy_previous_route
+**             | li_copy_last_path
+**             | li_remove_edges
+**                 | li_melt_paths
+**                     | li_edge_len
+**             | li_order_paths
+**         | li_eval_route
+**             | li_eval_steps
+**                 | li_eval_meanwhile
+**             | li_swap_paths
+**     | li_move_ants
+**         | li_allocate_ant_tab (*)
+**             | li_size_ttoa
+**                 | li_color_ants (O)
+**         | li_steps_length (O)
+**         | li_prepare_steps_str (O)
+**         | li_ants_distribution
+**         | li_display_steps (O)
+**         | aux_li_move_ants
+**             | li_get_output (R)
+**                 | ft_strlen (L)
+**                 | li_print_output
+**         | li_get_output (R)
+**             | ft_strlen (L)
+**             | li_print_output
+** | li_memory_liberator
+** | li_error
+** | li_print_output
+**
+** Note :
+** On aurait pu appeler 'li_eval_routes' apres chaque combinaison d'itineraires
+** decouverte, afin de s'arreter des lors qu'une combinaison n'exploitait pas
+** l'integralite de ses itineraires pour faire circuler les fourmis
+** Cependant, cela obligerait a allouer 'max_paths' et non 'path_nb' tableaux
+** dans la variable 'routes', car alors on ne saurait pas a l'avance combien
+** d'itineraires 'bhandari' identifierait
+**
+** Rappel :
+** Dans 'data->paths'
+** - Au terme de 'li_bhandari' :
+**   L'element d'index 'data->size' de chaque itineraire correspond l'index de
+**   la salle de depart
+** Dans 'data->routes'
+** - Au terme de 'li_build_routes' :
+**   L'element d'index 'data->size' de chaque itineraire correspond a l'index de
+**   la salle d'arrivee (la longueur de l'itineraire)
+** - Au terme de 'li_eval_routes' :
+**   L'element d'index 'data->size + 1' correspond au nombre de fourmis devant
+**   emprunter cet itineraire (valeur qui sera decrementee jusqu'a '0' lors
+**   de l'appel de 'li_distribute_ants')
+**
+** Exemple 01 (Absence de point de congestion):
+**
+** 4 fourmis:
+** A->B->C->J
+** A->D->E->F->J
+** A->G->H->I->J
+**
+** 1 chemin : 6 tours
+** 2 chemins : 6 tours
+** 3 chemins : 5 tours
+**
+** Reflexion :
+** S'il est strictement plus rapide de passer par 1 chemin que par 2, alors cela
+** signifie que le 2nd chemin n'est pas emprunte, et donc que la recherche d'un
+** eventuel 3eme chemin (necessairement de longueure >= au 2eme) est derisoire
+** Note :
+** Ne pas omettre que la decouverte d'un nouveau chemin, en raison de sa
+** superposition potentielle avec les chemins precedemment decouverts, peut
+** entrainer une deviation provoquant rallongement de ces derniers !
+**
+**
+** Soit A le nombre de fourmis a devoir traverser
+**
+** Soit L(n) la longueur du plus long chemin decouvert lors de l'etape n
+**
+** Soit S(n) le nombre de fourmis total ayant traverse la fourmiliere de part en
+** part le temps que la 1ere fourmi ayant emprunte le plus long itineraire
+** atteigne la sortie
+** Pour n = 1 :
+** S(1) = 1
+** Pour n > 1 :
+** S(n) = S(n - 1) + [(L(n) - L(n - 1)) * (n - 1)] + 1
+**
+** Pour n > 1, soit D(n) la difference entre S(n) et S(n - 1)
+** D(n) = [(L(n) - L(n - 1)) * (n - 1)] + 1
+**
+** La combinaison de chemin de l'etape 'n' est parfaitement exploite ssi on a :
+** (A - S(n)) % n == 0
+** ie
+** A % n == S(n) % n
+**
+** Exemple 02 (Absence de point de congestion):
+**
+**   A % n            S(N) % n
+** 5 % 1 = 0    &    1 % 1 = 0
+** 5 % 2 = 1    &    3 % 2 = 1
+** 5 % 3 = 2    &    4 % 3 = 1
+** 5 % 4 = 1    &    5 % 4 = 1
+**
+** 5 fourmis:
+** A->B->Z
+** A->C->D->Z
+** A->E->F->Z
+** A->G->H->Z
+**
+** 1 chemin : 6 tours
+** 2 chemins : 5 tours
+** 3 chemins : 5 tours
+** 4 chemins : 4 tours
+**
+** Exemple 3 (Presence de point de congestion):
+**
+** On cherche un nombre de fourmis > 6 qui soit congru a 0 modulo 3
+** et qui ne soit pas congru a 1 modulo 2
+** ie
+** un multiple de 6 strictement superieur a 6
+**
+** 12 fourmis:
+**
+** 0  1  2  3
+** A->B->C->D->Z (12)
+** 15 etapes
+**
+** 0  1  2  3  4  5  6  7  8  9  10
+** A->B->C->D->E->F->G->H->I->J->Z (7)
+** A->B->C->D->E->F->G->H->I->J->K->Z (5)
+** 16 etapes
+**
+** 0  1  2  3  4  5  6  7  8  9  10 11
+** A->B->C->D->E->F->G->H->I->J->Z (5)
+** A->B->C->D->E->F->G->H->I->J->K->Z (4)
+** A->B->C->D->E->F->G->H->I->J->K->M->Z (3)
+** 14 etapes
+*/
+
+int		lem_in(t_data *data)
+{
+	int		ret_val;
+
+//	li_epur(data);
+	if (!li_bhandari(data))
+	{
+		return (0);
+	}
+	else
+	{
+		ret_val = li_move_ants(data);
+		li_print_output(data);
+		ft_putchar('\n');
+		ft_putstr("LI_PRINT_PATHS\n");
+		li_print_paths(data);
+		ft_putchar('\n');
+		ft_putstr("LI_PRINT_ROUTES\n");
+		li_print_routes(data);
+		ft_putchar('\n');
+		return (ret_val ? 1 : 0);
+	}
+}
+
+/*
+** 'data.ants' est initialisee dans 'li_match_ants'
+** 'data.size' est initialisee dans 'li_parse_rooms'
+** 'data.path_nb' est initialisee dans 'li_bhandari'
+** 'data.max_paths' est initialisee dans 'li_bhandari_max_iterations'
+** 'data.best_steps' est initialisee dans 'li_eval_routes'
+** 'data.best_route' est initialisee dans 'li_eval_routes'
+*/
+
+void	li_initialise_data(t_data *data)
+{
+	data->rooms = NULL;
+	data->map = NULL;
+	data->paths = NULL;
+	data->routes = NULL;
+	(data->output).index = 0;
+	data->ant_tab = NULL;
+}
+
+int		main(int argc, char **argv)
+{
+	t_data	data;
+	int		ret_val;
+
+	data.rooms = NULL;
+	data.map = NULL;
+	data.paths = NULL;
+	data.routes = NULL;
+	(data.output).index = 0;
+	data.ant_tab = NULL;
+	if (!(ret_val = (!li_options(&data, argc, argv) || data.help) ? 0 : 1))
+		data.help ? li_help() : li_usage();
+	else
+	{
+		ret_val = li_parse_input(&data);
+		li_print_output(&data);
+		if (!ret_val || !(ret_val = (!data.ants || lem_in(&data)) ? 1 : 0))
+			li_error();
+	}
+	li_memory_liberator(&data);
+	if (ret_val)
+		li_print_output(&data);
+//	li_stats(&data);
+	return (0);
+}
