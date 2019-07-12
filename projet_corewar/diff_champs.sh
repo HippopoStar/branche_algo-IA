b82e850e244e87b36c0b646c19dcc68d4ee561cd
Hephaistos <leocabanes@wanadoo.fr>
2019-07-12 20:29:38 +0200

    "diff_champs.sh" : compatibilite "sh"

--- projet_corewar/diff_champs.sh
+++ projet_corewar/diff_champs.sh
@@ -3,16 +3,20 @@
 MY_DIR='champs_binaries'
 REF_DIR='champs_binaries_by_zaz'
 
+# La commande 'let'
+# (OpenClassooms/Reprenez le controle a l'aide de Linux/Afficher et manipuler des variables)
+# fonctionne sous 'bash' mais pas sous 'sh'
+
 check_nb () {
-	let "a = `ls -1 ${REF_DIR}/*.cor | wc -l`"
-	let "b = `ls -1 ${MY_DIR}/*.cor | wc -l`"
-	let "c = a - b"
-	echo "ZaZ a compile ${c} champions de plus que toi !"
+	local REF_DIR_NB="$(ls -1 ${REF_DIR}/*.cor | wc -l)"
+	local MY_DIR_NB="$(ls -1 ${MY_DIR}/*.cor | wc -l)"
+	local DIFF="$((${REF_DIR_NB} - ${MY_DIR_NB}))"
+	echo "ZaZ a compile ${DIFF} champions de plus que toi !"
 }
 
 check_diff () {
 	ls -1 ${MY_DIR}/*.cor \
-		| awk -v ref_dir="${REF_DIR}" -v log="${LOG_FILE}" \
+		| awk -v ref_dir="${REF_DIR}" \
 			'{ print "echo ./" $0 "\ndiff " $0 " " ref_dir "/$(basename " $0 ")" }' \
 		| sh -s
 }
