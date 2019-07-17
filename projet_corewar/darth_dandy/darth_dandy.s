b75c2649c5f72099e7858b3a96dc269f06e9c3be
Hephaistos <leocabanes@wanadoo.fr>
2019-07-17 16:04:33 +0200

    Ebauche de "darth_dandy_v7"

--- projet_corewar/darth_dandy/darth_dandy.s
+++ projet_corewar/darth_dandy/darth_dandy.s
@@ -3,7 +3,12 @@
 
 begin:
 	sti r1, %:live, %1
-	sti r1, %:backpack, %1
+	sti r1, %:live1and3, %1
+	sti r1, %:live2and4, %1
+	sti r1, %:live5and7, %1
+	sti r1, %:live6and8, %1
+	sti r1, %:backpack1, %1
+	sti r1, %:backpack2, %1
 decale:
 	add r1, r1, r4 # 01
 	add r4, r4, r4 # 02
@@ -26,40 +31,90 @@ live:
 carry:
 	and r16, r16, r16
 fork1:
+	fork %:traveller2
 	fork %:fork2 # Le 1er process se duplique une premiere fois
+live1and3:
+	live %42
 	fork %:wait3 # Le 1er process se duplique une seconde fois
 	zjmp %:load1 # Le 1er process va se preparer a l'expedition
 fork2:
+live2and4:
+	live %42
 	fork %:wait4 # Le 2e process se duplique
 	zjmp %:load2 # Le 2e process va se preparer a l'expedition
 wait3:
 	zjmp %:load3 # Le 3e process attend le 1er et va se preparer
 wait4:
 	zjmp %:load4 # Le 4e process attend le 2e et va se preparer
+
+# Traveller 1
 load1:
 	ld %190054915, r2
-	ld %-496, r3
+	ld %-64, r3
 	and r16, r16, r16
-	zjmp %:adventure
+	zjmp %:adventure1
 load2:
 	ld %251789311, r2
-	ld %-492, r3
+	ld %-60, r3
 	and r16, r16, r16
-	zjmp %:adventure
+	zjmp %:adventure1
 load3:
 	ld %2558, r2
 	add r2, r4, r2
-	ld %-488, r3
+	ld %-56, r3
 	and r16, r16, r16
-	zjmp %:adventure
+	zjmp %:adventure1
 load4:
-	ld %100663296, r2
-	ld %-484, r3
+	ld %1677721600, r2
+	ld %-52, r3
 	and r16, r16, r16
-	zjmp %:adventure
-adventure:
-	sti r2, r3, r16
-backpack:
-	live %42
-	zjmp %-506
+	zjmp %:adventure1
 
+traveller2:
+	fork %:fork6 # Le 1er process du 2e voyageur se duplique une premiere fois
+live5and7:
+	fork %:wait7 # Le 1er process du 2e voyageur se duplique une seconde fois
+	zjmp %:load5 # Le 1er process du 2e voyageur se preparer a l'expedition
+fork6:
+live6and8:
+	fork %:wait8 # Le 2e process du 2e voyageur se duplique
+	zjmp %:load6 # Le 2e process du 2e voyageur va se preparer a l'expedition
+wait7:
+	zjmp %:load7 # Le 3e process du 2e voyageur attend le 1er et va se preparer
+wait8:
+	zjmp %:load8 # Le 4e process du 2e voyageur attend le 2e et va se preparer
+
+# Traveller 2
+load5:
+	ld %190054915, r2
+	ld %64, r3
+	and r16, r16, r16
+	zjmp %:adventure2
+load6:
+	ld %251789311, r2
+	ld %68, r3
+	and r16, r16, r16
+	zjmp %:adventure2
+load7:
+	ld %2304, r2
+	add r2, r4, r2
+	ld %72, r3
+	and r16, r16, r16
+	zjmp %:adventure2
+load8:
+	ld %905969644, r2
+	ld %76, r3
+	and r16, r16, r16
+	zjmp %:adventure2
+
+adventure1:
+	sti r2, r3, r16
+backpack1:
+	live %42
+	zjmp %-74
+
+adventure2:
+	sti r2, r3, r16
+backpack2:
+	live %42
+	zjmp %54
